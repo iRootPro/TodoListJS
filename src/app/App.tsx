ef79003813463df41d0f0d1a88f4c902de5bf0f0
Alexander Neupokoev <admin@armavir.ru>
2020-10-14 15:51:02 +0300

    Add formik and react-router. Add validate

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,12 +8,15 @@ import {useSelector} from "react-redux";
 import {AppRootStateType} from "./store";
 import {RequestStatusType} from "./app-reducer";
 import {ErrorSnackbar} from "../components/ErrorSnackbar/ErrorSnackbar";
+import {Login} from "../features/Login/Login";
+import {BrowserRouter, Route} from 'react-router-dom'
 
 function App() {
 const status = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status)
 
     return (
         <div className="App">
+            <BrowserRouter>
             <AppBar position="static">
                 {status === 'loading' && <LinearProgress color="secondary"/>}
                 <Toolbar>
@@ -27,9 +30,11 @@ const status = useSelector<AppRootStateType, RequestStatusType>(state => state.a
                 </Toolbar>
             </AppBar>
             <Container fixed>
-                <TodolistsList/>
+                    <Route exact path={'/'} render={() => <TodolistsList/> }/>
+                    <Route path={'/login'} render={() => <Login/>}/>
             </Container>
             <ErrorSnackbar/>
+            </BrowserRouter>
         </div>
     )
 }
---
+++ src/features/Login/Login.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import {Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, TextField, Button, Grid} from '@material-ui/core'
+import {useFormik} from "formik";
+
+export const Login = () => {
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values));
+        },
+        validate: values => {
+            if (!values.email)
+                return {
+                    email: 'Email is required'
+                }
+            if (!values.password)
+                return {
+                    password: 'Password is required'
+                }
+        }
+    });
+
+    return <Grid container justify="center">
+        <Grid item xs={4}>
+            <form onSubmit={formik.handleSubmit}>
+            <FormControl>
+                <FormLabel>
+                    <p>To log in get registered
+                        <a href={'https://social-network.samuraijs.com/'}
+                           target={'_blank'}>here
+                        </a>
+                    </p>
+                    <p>or use common test account credentials:</p>
+                    <p>Email: free@samuraijs.com</p>
+                    <p>Password: free</p>
+                </FormLabel>
+                <FormGroup>
+                    <TextField
+                        label="Email"
+                        margin="normal"
+                        {...formik.getFieldProps('email')}
+                    />
+                    {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+                    <TextField
+                        type="password"
+                        label="Password"
+                        margin="normal"
+                        {...formik.getFieldProps('password')}
+                    />
+                    {formik.errors.password ? <div>{formik.errors.password}</div> : null}
+                    <FormControlLabel
+                        label={'Remember me'}
+                        control={<Checkbox
+                            {...formik.getFieldProps('rememberMe')}
+                            checked={formik.values.rememberMe}
+                        />}
+                    />
+                    <Button type={'submit'} variant={'contained'} color={'primary'}>Login</Button>
+                </FormGroup>
+            </FormControl>
+            </form>
+        </Grid>
+    </Grid>
+}
